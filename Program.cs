6dc219828135552020cf7e16b63f33c159cdcf0e
UIT-20521205 <dinhiendung10@gmail.com>
2023-04-01 21:01:07 +0700

    Complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Hello;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+
+        string yourName = Console.ReadLine();
+
+        Console.WriteLine("Hello: " + yourName);
+    }
+}
